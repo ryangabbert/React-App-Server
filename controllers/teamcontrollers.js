87686a669b41a0be84215e682708a2e4c8cc5e16
Ryan Gabbert <rgabb08@gmail.com>
2018-07-03 13:08:18 -0400

    changed team controller further

--- controllers/teamcontrollers.js
+++ controllers/teamcontrollers.js
@@ -7,7 +7,7 @@ router.post('/createteam', (req, res) => {
    Team.create({
         player_name: req.body.Team.player_name,
         email: req.body.Team.email,
-        postion: req.body.Team.postion
+        position: req.body.Team.position
     })
     .then(team => res.status(200).json(team))
     .catch(err => res.status(500).json({error: err.errors[0].message}))
