4a840bac8d52ffbe0b3f68cd01011cd7a83b8307
Ryan Gabbert <rgabb08@gmail.com>
2018-07-03 17:08:17 -0400

    fixed bugs

--- controllers/gamescontroller.js
+++ controllers/gamescontroller.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const sequelize = require('../db')
 const Games = sequelize.import('../models/games');
 
-router.get('/', (req, res) => {
+router.get('/getall', (req, res) => {
     Games.findAll()
         .then(games => res.status(200).json(games))
         .catch(err => res.status(500).json({error: err.errors[0].message}))
