0cee0f33767fc93a3f26674edbad47bb27a95e67
Ryan Gabbert <rgabb08@gmail.com>
2018-07-03 13:04:37 -0400

    added to team controllers

--- controllers/teamcontrollers.js
+++ controllers/teamcontrollers.js
@@ -5,9 +5,9 @@ const Team = sequelize.import('../models/team');
 
 router.post('/createteam', (req, res) => {
    Team.create({
-        player_name: req.body.player_name,
-        email: req.body.email,
-        postion: req.body.postion
+        player_name: req.body.Team.player_name,
+        email: req.body.Team.email,
+        postion: req.body.Team.postion
     })
     .then(team => res.status(200).json(team))
     .catch(err => res.status(500).json({error: err.errors[0].message}))
