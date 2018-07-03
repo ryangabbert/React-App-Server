a611f6cd5b38af2eff47a3ed78dad497f0f12bbc
Ryan Gabbert <rgabb08@gmail.com>
2018-07-03 17:01:11 -0400

    added fetch all

--- controllers/gamescontroller.js
+++ controllers/gamescontroller.js
@@ -2,6 +2,11 @@ const router = require('express').Router();
 const sequelize = require('../db')
 const Games = sequelize.import('../models/games');
 
+router.get('/', (req, res) => {
+    Games.findAll()
+        .then(games => res.status(200).json(games))
+        .catch(err => res.status(500).json({error: err.errors[0].message}))
+})
 
 router.post('/creategame', (req, res) => {
    Games.create({
