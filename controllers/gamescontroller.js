373bf7b1fb7747214645aa130f7a0327ab4ae906
Ryan Gabbert <rgabb08@gmail.com>
2018-07-05 20:35:53 -0400

    controller

--- controllers/gamescontroller.js
+++ controllers/gamescontroller.js
@@ -20,26 +20,27 @@ router.post('/creategame', (req, res) => {
     .catch(err => res.status(500).json({error: err.errors[0].message}))
 }) 
 router.put('/updategame/:id', (req, res) => {
-        var games = req.params.id; //3
-        var Games = req.body.Games.item; //4
-    
+        var games = req.params.id; 
+        var Location = req.body.Games.location_of_game; 
+        var Time = req.body.Games.time_of_game; 
+        var Date = req.body.Games.date_of_game; 
+        var Description = req.body.Games.description; 
+
         Games
             .update({ 
-                id: req.body.Games.id,
-                location_of_game: req.body.Games.location_of_game,
-                time_of_game: req.body.Games.time_of_game,
-                date_of_game: req.body.Games.date_of_game,
-                description: req.body.Games.description
+                Location: location_of_game,
+                Time: time_of_game,
+                Date: date_of_game,
+                Description: description
             },
             {where: {id: games}} 
             ).then(
                 function updateSuccess(updatedGames) { 
                     res.json({
-                        id: req.body.Games.id,
-                        location_of_game: req.body.Games.location_of_game,
-                        time_of_game: req.body.Games.time_of_game,
-                        date_of_game: req.body.Games.date_of_game,
-                        description: req.body.Games.description
+                        Location: location_of_game,
+                        Time: time_of_game,
+                         Date: date_of_game,
+                        Description: description
                     });            
                 },
                 function updateError(err){ 
